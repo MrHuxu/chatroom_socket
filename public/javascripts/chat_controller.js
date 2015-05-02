afd77e4810977be1a5c6846df296158de4ce05c6
Xu Hu <hxtheone@gmail.com>
2015-05-02 18:35:36 +0800

    this app can work now

---
+++ public/javascripts/chat_controller.js
@@ -0,0 +1,29 @@
+ChatRoomModule.controller('chat_controller', function ($scope, $modal, $log, socket) {
+  $scope.test = 'world';
+  $scope.messages = [];
+
+  socket.on('chat message', function (msg) {
+    $scope.messages.push(msg);
+  });
+
+  var modalInstance = $modal.open({
+    templateUrl: '/templates/input_nickname.ejs',
+    size: 'sm',
+    controller: 'ModalInstanceCtrl'
+  });
+
+  modalInstance.result.then(function (nickname) {
+    $scope.nickname = nickname || 'Anonymous';
+  });
+
+  $scope.send = function (message) {
+    socket.emit('chat message', $scope.nickname + ': ' + message);
+    $scope.message = '';
+  };
+});
+
+ChatRoomModule.controller('ModalInstanceCtrl', function ($scope, $modalInstance) {
+  $scope.save_nickname = function () {
+    $modalInstance.close($scope.nickname);
+  };
+});
--- public/javascripts/chat_room_module.js
+++ public/javascripts/chat_room_module.js
@@ -1,4 +1,4 @@
-var ChatRoomModule = angular.module('chat_room_module', []);
+var ChatRoomModule = angular.module('chat_room_module', ['ui.bootstrap', 'luegg.directives']);
 
 ChatRoomModule.factory('socket', function ($rootScope) {
   var socket = io.connect();
@@ -21,19 +21,5 @@ ChatRoomModule.factory('socket', function ($rootScope) {
         });
       })
     }
-  };
-});
-
-ChatRoomModule.controller('chat_controller', function ($scope, socket) {
-  $scope.test = 'world';
-  $scope.messages = [];
-
-  socket.on('chat message', function (msg) {
-    $scope.messages.push(msg);
-  });
-
-  $scope.send = function (message) {
-    socket.emit('chat message', message);
-    $scope.message = '';
   };
 });
