d4b425741708c923091f2622c687211c131d5fde
Xu Hu <hxtheone@gmail.com>
2015-05-06 01:21:58 +0800

    nearly finish private chat

--- public/javascripts/chat_controller.js
+++ public/javascripts/chat_controller.js
@@ -1,6 +1,7 @@
 ChatRoomModule.controller('chat_controller', function ($scope, $modal, $log, socket) {
   $scope.messages = [];
   $scope.users = [];
+  $scope.sendTo = 'global';
 
   var modalInstance = $modal.open({
     templateUrl : '/templates/input_nickname.ejs',
@@ -13,14 +14,33 @@ ChatRoomModule.controller('chat_controller', function ($scope, $modal, $log, soc
     $scope.nickname = nickname;
   });
 
+  $scope.changeTarget = function (nickname) {
+    $scope.sendTo = nickname;
+  };
+
   $scope.send = function (message) {
     message = message || '';
-    socket.emit('chat message', [$scope.nickname, message]);
+    socket.emit('chat message', {
+      name    : $scope.nickname,
+      message : message,
+      sendTo  : $scope.sendTo
+    });
     $scope.message = '';
   };
 
   socket.on('chat message', function (msg) {
-    $scope.messages.push(msg[0] + ': ' + msg[1]);
+    var content = msg.name + ': ' + msg.message;
+    if (msg.name === $scope.nickname) {
+      if (msg.sendTo !== 'global')
+        $scope.messages.push('[to ' + msg.sendTo + '] ' + content);
+      else
+        $scope.messages.push(content);
+    } else {
+      if (msg.sendTo === $scope.nickname)
+        $scope.messages.push('[private] ' + content);
+      else if (msg.sendTo === 'global')
+        $scope.messages.push(content);
+    }
   });
 
   socket.on('init users', function (users) {
